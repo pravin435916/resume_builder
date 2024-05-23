3708e61fcb25e36ad84fe1b1d20819ee7ccbc782
pravin nandankar <pravinnandankar03@gmail.com>
2024-05-23 17:53:32 +0530

    responsivesness

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -12,17 +12,17 @@ const Navbar = () => {
     const { user, handleSignOut } = useFirebase();
 
     return (
-        <nav className="w-full z-10 bg-gray-100 shadow-md">
-            <div className=" text-black py-4 px-6 flex justify-between items-center gap-4 mx-8">
+        <nav className="w-full z-10 bg-gray-100 shadow-md overflow-hidden">
+            <div className=" text-black py-4 sm:px-6 flex justify-between items-center gap-4 mx-8">
                 <img className="w-12" src="https://pro2-bar-s3-cdn-cf.myportfolio.com/3d13047a-c0fe-40bf-aeca-94fdf975291e/84193e9e-b5a2-4499-a4ec-7c4750826db0_rwc_0x614x5004x3753x5004.png?h=c79866de72d6dc2b3cc77502e03fc104" alt="" />
-                <input type="search" className="rounded-md p-2 outline-none w-44 md:w-[50rem]" placeholder="Search..." />
+                <input type="search" className="rounded-md p-2 outline-none w-24 sm:w-[50rem]" placeholder="Search..." />
                 <div>
                     {user ? (
                         <img className="w-12 h-12 border rounded-full cursor-pointer" onClick={popUP} src={user.photoURL} alt="User Profile" />
                     ) : (
                         <Link className="flex gap-2" to="/auth">
-                            <div className="flex gap-2 items-center rounded-2xl text-white px-4 py-2 font-semibold bg-black">
-                                <span>Sign in</span>
+                            <div className="flex gap-2 items-center rounded-2xl text-white sm:px-4 sm:py-2 p-2 font-semibold bg-black">
+                                <span >Sign in</span>
                                 <span className="text-2xl"><LiaSignInAltSolid /></span>
                             </div> </Link>
                     )}
